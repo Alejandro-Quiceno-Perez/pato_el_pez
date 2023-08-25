3e234613c5a521fb3f03a8cd1fd171c5aac9c056
Alejandro Quiceno Pérez <alejo.perez2905@gmail.com>
2023-08-25 00:29:29 -0500

    Se creo el boton para ir arriba de la pagina

--- Components/animatios.js
+++ Components/animatios.js
@@ -1,17 +1,20 @@
-window.sr = ScrollReveal();
+// window.sr = ScrollReveal();
 
-sr.reveal('.container-fluid', {
-    duration: 3000,
-    origin: 'bottom',
+// sr.reveal('.container-fluid', {
+//     duration: 3000,
+//     origin: 'bottom',
     
-});
+// });
 
-sr.reveal('.seccion1', {
-    duration: 6000,
-    origin: 'bottom',
-});
-sr.reveal('.seccion2', {
-    duration: 6000,
-    origin: 'left',
-});
+// sr.reveal('.seccion1', {
+//     duration: 6000,
+//     origin: 'bottom',
+// });
+// sr.reveal('.seccion2', {
+//     duration: 6000,
+//     origin: 'left',
+// });
 
+document.querySelector('.btn-banda').addEventListener('click', () => {
+    document.querySelector('.seccion2');
+});
